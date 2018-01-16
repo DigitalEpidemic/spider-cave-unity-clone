0d172a925dc9704776fa59bc7f4be571fdfe1484
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-16 16:12:00 -0500

    Added Main and Level menus

---
+++ Assets/Scripts/Game Controllers/LevelController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour {
+
+	public void PlayGame () {
+		SceneManager.LoadScene ("Gameplay");
+	}
+
+	public void BackToMenu () {
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+} // LevelController
