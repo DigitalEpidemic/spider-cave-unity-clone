fdbc558f8ccc7a17a355bad52821c2aea65c6d79
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-16 17:04:38 -0500

    Added Camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform player;
+
+	public float minX, maxX;
+
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+	}
+
+	void Update () {
+		if (player != null) {
+			Vector3 temp = transform.position;
+			temp.x = player.position.x;
+
+			if (temp.x < minX) {
+				temp.x = minX;
+			}
+
+			if (temp.x > maxX) {
+				temp.x = maxX;
+			}
+
+			temp.y = player.position.y + 3.2f;
+			transform.position = temp;
+		}
+	}
+
+} // CameraFollow
