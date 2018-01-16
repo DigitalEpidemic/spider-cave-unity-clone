b2d61909ebe1a0a230bafa80f88ca52a49aa17e8
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-16 03:06:19 -0500

    Added Spider Shooter and Spider Jumper

---
+++ Assets/Scripts/Spider/Shooter/SpiderBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderBullet : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "Player") {
+			Destroy (target.gameObject);
+			Destroy (gameObject);
+		}
+
+		if (target.tag == "Ground") {
+			Destroy (gameObject);
+		}
+	}
+
+} // SpiderBullet
---
+++ Assets/Scripts/Spider/Shooter/SpiderShooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderShooter : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject bullet;
+
+	void Start () {
+		StartCoroutine (Attack ());
+	}
+
+	IEnumerator Attack () {
+		yield return new WaitForSeconds (Random.Range (2, 7));
+		Instantiate (bullet, transform.position, Quaternion.identity);
+		StartCoroutine (Attack ());
+	}
+
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "Player") {
+			Destroy (target.gameObject);
+		}
+	}
+
+} // SpiderShooter
