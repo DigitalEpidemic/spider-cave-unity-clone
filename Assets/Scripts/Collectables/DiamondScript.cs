f1fcd2df68360affcfe7304bec5046c258ccf493
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-16 15:37:06 -0500

    Added door and diamond collectables

---
+++ Assets/Scripts/Collectables/DiamondScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondScript : MonoBehaviour {
+
+
+
+	void Start () {
+		if (Door.instance != null) {
+			Door.instance.collectablesCount++;
+		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "Player") {
+			Destroy (gameObject);
+			if (Door.instance != null) {
+				Door.instance.DecrementCollectables ();
+			}
+		}
+	}
+
+} // DiamondScript
---
+++ Assets/Scripts/Door/Door.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public static Door instance;
+
+	private BoxCollider2D box;
+	private Animator anim;
+
+	[HideInInspector]
+	public int collectablesCount;
+
+	void Awake () {
+		MakeInstance ();
+		anim = GetComponent<Animator> ();
+		box = GetComponent<BoxCollider2D> ();
+	}
+
+	void MakeInstance () {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	public void DecrementCollectables () {
+		collectablesCount--;
+
+		if (collectablesCount == 0) {
+			StartCoroutine (OpenDoor ());
+		}
+	}
+	
+	IEnumerator OpenDoor () {
+		anim.Play ("Open");
+		yield return new WaitForSeconds (0.7f);
+		box.isTrigger = true;
+	}
+
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "Player") {
+		}
+		Debug.Log ("Game Finished");
+	}
+
+} // Door
